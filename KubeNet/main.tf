68537e5133311e3b5562917bc70caa5e988853a2
Oreire <lancerajayi@yahoo.com>
2025-03-20 03:50:01 +0000

    Update main.tf 4
    let us see

--- KubeNet/main.tf
+++ KubeNet/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "k8s_nodes" {
     from_port   = 0
     to_port     = 65535
     protocol    = "-1"
-    cidr_blocks = [aws_vpc.clust-net.cidr_block]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # Allow SSH access from my local IP
